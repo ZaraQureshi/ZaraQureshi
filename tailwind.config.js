5bcfa1984c792029686431538d503b2ff54096bf
ZaraQureshi <qzara819@gmail.com>
2025-11-17 08:04:38 +0530

    working on adding dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // enables dark mode by adding the class "dark"
   content: [
-    "./src/components/**/*.tsx",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          light: "#fafafa",
+          dark: "#000000",
+        },
+        surface: {
+          light: "#ffffff",
+          dark: "#1f1f1f",
+        },
+        accent: {
+          light: "#db2777", // pink-600
+          dark: "#ec4899", // pink-500
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
