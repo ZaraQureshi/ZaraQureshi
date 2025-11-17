29f3a0f754e3f6e83b6c312e6b2d8a7f094b8717
ZaraQureshi <qzara819@gmail.com>
2025-11-17 13:49:54 +0530

    removed code that was causing buid error

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,15 +3,15 @@ import zara from "../../public/info.json";
 import {  Mail, Github, LinkedinIcon, ArrowUpRight } from "lucide-react";
 
 function Card() {
-  const [theme, setTheme] = useState("light");
+  // const [theme, setTheme] = useState("light");
 
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setTheme(savedTheme);
-      document.documentElement.classList.toggle("dark", savedTheme === "dark");
-    }
-  }, []);
+  // useEffect(() => {
+  //   const savedTheme = localStorage.getItem("theme");
+  //   if (savedTheme) {
+  //     // setTheme(savedTheme);
+  //     document.documentElement.classList.toggle("dark", savedTheme === "dark");
+  //   }
+  // }, []);
 
   // const toggleTheme = () => {
   //   const newTheme = theme === "light" ? "dark" : "light";
