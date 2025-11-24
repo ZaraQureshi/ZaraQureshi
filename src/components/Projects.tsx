1ee373b9513f6c904a4df804c3f237b62a14d9b9
ZaraQureshi <qzara819@gmail.com>
2025-11-24 14:52:36 +0530

    modified Projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,29 +1,64 @@
+import { ArrowUpRight, Github } from 'lucide-react'
 import zara from '../../public/info.json'
-export const Projects=()=>{
+import { Header } from './Header'
+export const Projects = () => {
     console.log("projects")
-    return(
-        <div>
-            <h2 className="text-2xl font-bold mb-2 text-pink-400">Projects</h2>
+    return (
+        <>
 
-          <div className="">
-            {zara.zara.projects.slice(0, 3).map((proj) => (
-              <div
-                key={proj.projectName}
-                className="flex flex-col justify-between h-full shadow-lg bg-gradient-to-br from-gray-900 to-gray-800 rounded-xl p-6"
-              >
-                <div>
-                  <p className="text-lg font-semibold text-white">{proj.projectName}</p>
-                  <p className="text-sm text-gray-300 mt-2">{proj.projectDescription}</p>
-                </div><div>
-                  <p className="text-xs text-gray-400 my-2">Tech: {proj.techStack.join(", ")}</p>
+            <div className="min-h-screen w-[100vw] 
+            bg-gradient-to-br from-gray-900 to-gray-800 
+            p-4 sm:p-4 
+            ">
+                <div className="m-8">
 
-                 
+                <Header />
                 </div>
-              </div>
-            ))}
+                <div className="flex items-center justify-center m-10">
+                    <div className="w-full max-w-4xl ">
+                        <h2 className="text-4xl font-bold mb-6 text-pink-400 text-center">
+                            Projects
+                        </h2>
+                        <span className="  italic 
+                                    text-pink-300 
+                                    font-medium 
+                                    tracking-wide 
+                                    bg-gradient-to-r from-pink-600/20 to-purple-600/20 
+                                    px-3 py-1 mt-10
+                                    rounded-md">“Built with curiosity. Improved with persistence.”</span>
+                        {zara.zara.projects.slice(0, 3).map((proj) => (
+                            <div
+                                key={proj.projectName}
+                                className="text-left h-full lg:m-10 m-5"
+                            >
+                                <div className="flex items-center"><span className="text-lg font-semibold text-white">{proj.projectName}</span><a className="text-lg font-semibold text-white pl-2 o" href={`${proj.githubLink}`}><Github className="text-pink-500 h-5" /></a>|<a className="text-lg font-semibold text-white" href={`${proj.projectLink}`}><ArrowUpRight className="text-pink-500 h-5" /></a></div>
+                                <div>
+                                    <p className="text-sm text-gray-300 mt-2 text-left">{proj.projectDescription}</p>
+                                    <div className="flex flex-wrap gap-2 mt-2">
+                                        {proj.techStack.map((tech, i) => (
+                                            <span
+                                                key={i}
+                                                className="badge"
+                                            >
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
+                </div>
 
 
-          </div>
-        </div>
+
+
+
+
+
+                {/* </div> */}
+            </div>
+        </>
     )
 }
